e2b9e3fb76c2f04fbb5bd09832e122158686a2dc
Samuel <kskoomson1@gmail.com>
2023-03-09 04:57:53 -0800

    printing last digits of random numbers but some checks failed. wrong output and pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,4 +6,4 @@ if number > 5:
 elif number == 0:
     print(f"Last digit of {number} is {number} and is 0")
 elif number < 6 and number != 0:
-    print(f"Last digit of {number} is {number} and is less than 6 and not zero")
+    print(f"Last digit of {number} is {number} and is less than 6 and not 0")
