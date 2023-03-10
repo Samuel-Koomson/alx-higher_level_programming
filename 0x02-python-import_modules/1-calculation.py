dd5793d6915e05e9223f9d88869c2504d98c1c1c
Samuel <kskoomson1@gmail.com>
2023-03-10 22:51:53 +0000

    Requirement failed so I relooked at the commenting

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,9 +2,7 @@
 # 1-calculation.py
 
 if __name__ == "__main__":
-    """This program does basic maths operations. addition, subtraction,
-    multiplication and division using the values  of 10 and 5, and makes 
-    use of imported functions in the calculator_1 module"""
+    """This program does basic maths operations. Add, sub, mul, div"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
