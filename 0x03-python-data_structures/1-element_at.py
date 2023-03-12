a3351762786cd9babfefd87a12dd8a73806e1797
Samuel <kskoomson1@gmail.com>
2023-03-12 19:28:00 +0000

    There was a for loop in the code before the if statement which was not needed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Function that retrieves elements from a list"""
 def element_at(my_list, idx):
-    for x in my_list:
-        if idx < 0 or idx >= len(my_list):
+	if idx < 0 or idx >= len(my_list) -1):
             return None
-   	return my_list[idx]
+   	return (my_list[idx])
     
