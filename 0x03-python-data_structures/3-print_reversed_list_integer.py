88de766dc2568225f9dd3e44ddff64f2fca22bdb
Samuel <kskoomson1@gmail.com>
2023-03-12 22:29:10 +0000

    Function that prints a list in reverse putting one integer per line

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    print(f"Printing a list in reverse {():d} {len(my_list)}")
+    print(f"Printing a list in reverse "{:d}".format(my_list))
