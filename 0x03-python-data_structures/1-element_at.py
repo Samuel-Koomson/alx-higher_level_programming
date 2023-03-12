243c4073a2d99b7364c4c5d9256111f4e46bb626
Samuel <kskoomson1@gmail.com>
2023-03-12 19:39:12 +0000

    There was a = to sign after the greater than operator in the if statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Function that retrieves elements from a list"""
 def element_at(my_list, idx):
+"""Function that retrieves elements from a list"""
 	if idx < 0 or idx > len(my_list) -1):
             return None
    	return (my_list[idx])
