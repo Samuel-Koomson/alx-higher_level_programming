b8834299c0918e7c95426458da6ff5d4c40a98a8
Samuel <kskoomson1@gmail.com>
2023-03-12 19:10:45 +0000

    There were issues with the return statements, in the situation where the function is greater than the list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Function that retrieves elements from a list"""
 def element_at(my_list, idx):
-    for idx in my_list:
-        if idx < 0:
+    for x in my_list:
+        if idx < 0 or idx >= len(my_list):
             return None
-    for idx in my_list:
-        if idx > my_list:
-            return None
+   	return my_list[idx]
     
