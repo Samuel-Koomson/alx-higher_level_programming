70b23e0ff8f44fd8cde3015d11bd3043d1e7d409
Samuel <kskoomson1@gmail.com>
2023-03-13 16:22:35 +0000

    Alternative code used to correct the errors in the code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
-#if tuples have less than 2 elements, add zeros to them
-    a = tuple_a + (0, 0)[:len(tuple_a)-2]
-    b = tuple_b + (0, 0)[:len(tuple_b)-2]
-    
-#return the sum of the two tuples matching the indices of the first two elements in each tuple
-    return(a[0] + b[0], a[1] + b[1])
+   
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
