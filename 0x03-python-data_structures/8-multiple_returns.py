885c9c19450d84674a953d08fd8270374ae03a25
Samuel <kskoomson1@gmail.com>
2023-03-13 18:02:25 +0000

    correcting task 8 and pushing it together with task 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    
-    if len(my_list) == 0:
-        return (None)
-
-    biggest = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > biggest:
-            biggest = my_list[i]
-
-    return (biggest)
+def multiple_returns(sentence):
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
