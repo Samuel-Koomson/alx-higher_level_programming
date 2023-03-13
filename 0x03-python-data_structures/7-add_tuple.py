52a0e6d411c755802c67123f312dbc730908c8f1
Samuel <kskoomson1@gmail.com>
2023-03-13 16:16:53 +0000

    Function code error in some sections, particularly with the return code of the function

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     
 #if tuples have less than 2 elements, add zeros to them
-    x = tuple_a + (0, 0)[:len(tuple_a)-2]
-    y = tuple_b + (0, 0)[:len(tuple_b)-2]
+    a = tuple_a + (0, 0)[:len(tuple_a)-2]
+    b = tuple_b + (0, 0)[:len(tuple_b)-2]
     
 #return the sum of the two tuples matching the indices of the first two elements in each tuple
-    return(x[0] + y[0], x[1] + y[1])
+    return(a[0] + b[0], a[1] + b[1])
