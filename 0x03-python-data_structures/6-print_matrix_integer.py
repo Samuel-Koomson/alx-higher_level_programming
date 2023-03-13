ec5e193a96d3f74ee3df85376ecd9221ff03ff4f
Samuel <kskoomson1@gmail.com>
2023-03-13 14:55:57 +0000

    Some outputs did not work so code was modified

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for column in matrix:
-        column_str = ''
-        for element in column:
-            column += '{:d} '.format(element)
-        print(column_str[:-1])
+    for index, num in enumerate(matrix):
+        # length = len(matrix[index]) - 1
+
+        for j, digit in enumerate(matrix[index]):
+            # if j = length:
+            if digit == matrix[index][-1]:
+                print("{:d}".format(digit), end='')
+            else:
+                print("{:d} ".format(digit), end='')
+        print()
