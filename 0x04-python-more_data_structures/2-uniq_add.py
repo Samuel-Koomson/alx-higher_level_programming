3cd3d032a777a1e369393d6a034fd10671b4a0e7
Samuel <kskoomson1@gmail.com>
2023-03-17 09:33:59 +0000

    Function that create common set to hold common elements

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    
+def uniq_add(my_list=[]):    
     unique_set = set()
-
     for num in my_list:
         if num not in unique_set:
             unique_set.add(num)
