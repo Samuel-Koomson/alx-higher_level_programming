230233d4bbc4ccfbe7ce6c106317861b52b77f08
Samuel <kskoomson1@gmail.com>
2023-04-12 05:14:18 +0000

    class base geometry that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function define base geometry class"""
+
+
+class BaseGeometry():
+    """base geometry"""
+    
+    def area(self):
+        """unimplemented method"""
+        raise Exception("area() is not implemented")
